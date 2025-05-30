0e8b0ae2b64d7c4a4b090a521280e9b04170fa73
oskoi <cdskv@proton.me>
2025-05-30 13:41:42 +0300

    add event builders

--- monotone.go
+++ monotone.go
@@ -22,9 +22,9 @@ var (
 	monotone_free    func(unsafe.Pointer) int
 	monotone_open    func(unsafe.Pointer, string) int
 	monotone_error   func(unsafe.Pointer) string
-	monotone_write   func(unsafe.Pointer, []MonotoneEvent, int) int
-	monotone_cursor  func(unsafe.Pointer, unsafe.Pointer, *MonotoneEvent) unsafe.Pointer
-	monotone_read    func(unsafe.Pointer, *MonotoneEvent) int
+	monotone_write   func(unsafe.Pointer, []Event, int) int
+	monotone_cursor  func(unsafe.Pointer, unsafe.Pointer, *Event) unsafe.Pointer
+	monotone_read    func(unsafe.Pointer, *Event) int
 	monotone_next    func(unsafe.Pointer) int
 	monotone_execute func(unsafe.Pointer, string, *unsafe.Pointer) int
 )
@@ -52,7 +52,33 @@ func init() {
 	purego.RegisterLibFunc(&monotone_execute, libmonotone, "monotone_execute")
 }
 
-type MonotoneEvent struct {
+const (
+	FlagWrite  = 0
+	FlagDelete = 1
+)
+
+func NewWriteEvent(id uint64, key string, value string) Event {
+	return newEvent(FlagWrite, id, key, value)
+}
+
+func NewDeleteEvent(id uint64, key string, value string) Event {
+	return newEvent(FlagDelete, id, key, value)
+}
+
+func newEvent(flags int, id uint64, key string, value string) Event {
+	keyPtr := unsafe.Pointer(unsafe.StringData(key))
+	valuePtr := unsafe.Pointer(unsafe.StringData(value))
+	return Event{
+		Flags:     flags,
+		Id:        id,
+		Key:       keyPtr,
+		KeySize:   uint64(len(key)),
+		Value:     valuePtr,
+		ValueSize: uint64(len(value)),
+	}
+}
+
+type Event struct {
 	Flags     int
 	Id        uint64
 	Key       unsafe.Pointer
@@ -80,7 +106,7 @@ func (m *Monotone) Open(s string) error {
 	return nil
 }
 
-func (m *Monotone) Write(batch []MonotoneEvent) error {
+func (m *Monotone) Write(batch []Event) error {
 	rc := monotone_write(m.env, batch, len(batch))
 	if rc == -1 {
 		return m.Error()
@@ -88,26 +114,24 @@ func (m *Monotone) Write(batch []MonotoneEvent) error {
 	return nil
 }
 
-func (m *Monotone) Cursor(key MonotoneEvent) (*MonotoneCursor, error) {
-	cursor := monotone_cursor(m.env, nil, &key)
-	if cursor == nil {
+func (m *Monotone) Cursor(key Event) (*Cursor, error) {
+	cur := monotone_cursor(m.env, nil, &key)
+	if cur == nil {
 		return nil, m.Error()
 	}
-	return &MonotoneCursor{
-		monotone: m,
-		cursor:   cursor,
+	return &Cursor{
+		env: m,
+		cur: cur,
 	}, nil
 }
 
 func (m *Monotone) Execute(command string) (string, error) {
-	var resultPtr unsafe.Pointer
-	rc := monotone_execute(m.env, command, &resultPtr)
+	var result unsafe.Pointer
+	rc := monotone_execute(m.env, command, &result)
 	if rc == -1 {
 		return "", m.Error()
 	}
-	result := goString(&resultPtr)
-	defer free(resultPtr)
-	return strings.Clone(result), nil
+	return goString(result), nil
 }
 
 func (m *Monotone) Error() error {
@@ -118,53 +142,54 @@ func (m *Monotone) Close() {
 	monotone_free(unsafe.Pointer(m.env))
 }
 
-type MonotoneCursor struct {
-	monotone *Monotone
-	cursor   unsafe.Pointer
-	total    int
+type Cursor struct {
+	env   *Monotone
+	cur   unsafe.Pointer
+	total int
 }
 
-func (m *MonotoneCursor) Read() (*MonotoneEvent, error) {
-	if m.total > 0 {
-		rc := monotone_next(m.cursor)
+func (c *Cursor) Read() (*Event, error) {
+	if c.total > 0 {
+		rc := monotone_next(c.cur)
 		if rc == -1 {
-			return nil, m.monotone.Error()
+			return nil, c.env.Error()
 		}
 	}
 
-	var event MonotoneEvent
-	rc := monotone_read(m.cursor, &event)
+	var event Event
+	rc := monotone_read(c.cur, &event)
 	if rc == -1 {
-		return nil, m.monotone.Error()
+		return nil, c.env.Error()
 	}
 	if rc == 0 {
 		return nil, io.EOF
 	}
 
-	m.total++
+	c.total++
 
 	return &event, nil
 }
 
-func (m *MonotoneCursor) Total() int {
-	return m.total
+func (c *Cursor) Total() int {
+	return c.total
 }
 
-func (m *MonotoneCursor) Close() {
-	monotone_free(unsafe.Pointer(m.cursor))
+func (c *Cursor) Close() {
+	monotone_free(unsafe.Pointer(c.cur))
 }
 
-func goString(ptr *unsafe.Pointer) string {
-	ptr2 := *ptr
-	if ptr2 == nil {
+func goString(ptr unsafe.Pointer) string {
+	if ptr == nil {
 		return ""
 	}
 	var length int
 	for {
-		if *(*byte)(unsafe.Add(ptr2, uintptr(length))) == '\x00' {
+		if *(*byte)(unsafe.Add(ptr, uintptr(length))) == '\x00' {
 			break
 		}
 		length++
 	}
-	return string(unsafe.Slice((*byte)(ptr2), length))
+	str := strings.Clone(string(unsafe.Slice((*byte)(ptr), length)))
+	free(ptr)
+	return str
 }
